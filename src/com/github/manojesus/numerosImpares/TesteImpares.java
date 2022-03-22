c085003b3321ba9aef78c9c933d035cbac9a9081
ManoJesus <lucas_748l@hotmail.com>
2022-03-22 17:53:34 -0300

    Algumas mudanças no teste dos impares

--- src/com/github/manojesus/numerosImpares/TesteImpares.java
+++ src/com/github/manojesus/numerosImpares/TesteImpares.java
@@ -8,6 +8,7 @@ public class TesteImpares {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("---------------- Imprime Impares System ----------------");
+        System.out.println("OBS: o valor inicial deve ser maior que 0 e menor que o valor final");
         System.out.println("Entre com o número inicial do intervalo: ");
         int inicio = sc.nextInt();
         System.out.println("Entre com o número final do intervalo: ");
@@ -20,6 +21,8 @@ public class TesteImpares {
                     System.out.print(i + " ");
                 }
             }
+        }else{
+            System.out.println("Erro na entrada de dados.");
         }
     }
 

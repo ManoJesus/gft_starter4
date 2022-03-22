8a031b689062c96e5a460e9363920782ed89c2d8
ManoJesus <lucas_748l@hotmail.com>
2022-03-22 17:00:30 -0300

    Primeiro commit com o desafio 1 - números impares concluido

---
+++ PrimeiroDesafio/src/com/github/manojesus/numerosImpares/TesteImpares.java
@@ -0,0 +1,26 @@
+package com.github.manojesus.numerosImpares;
+
+import java.util.Scanner;
+
+public class TesteImpares {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("---------------- Imprime Impares System ----------------");
+        System.out.println("Entre com o número inicial do intervalo: ");
+        int inicio = sc.nextInt();
+        System.out.println("Entre com o número final do intervalo: ");
+        int termino = sc.nextInt();
+
+        if(inicio > 0 && inicio<termino){
+            System.out.print("Aqui estão todos os números impares: ");
+            for(int i = inicio; i <= termino ; i++){
+                if(i % 2 != 0){
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
+
+}
